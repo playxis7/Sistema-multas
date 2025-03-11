8c872cbe4de73263bfbcffff8651b1d827700265
playxis7 <brunocromero246@gmail.com>
2025-03-11 18:09:45 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,23 @@
+const Orders = [
+    {
+        productName: 'Adriano Barbosa',
+        productNumber: '1F9936334',
+        productPlaca: 'BDY-1F34',
+        productData: '08/02/2025',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Adalcino Faria da Cunha',
+        productNumber: 'S019976556',
+        productPlaca: 'LZX-3868',
+        productData: '25/01/2025',
+        status: 'Vencida'
+    },
+    {
+        productName: 'Ademir de Oliveira',
+        productNumber: '1G1086034',
+        productPlaca: 'BDY-1F47',
+        productData: '11/03/2025',
+        status: 'Paga'
+    },
+]
